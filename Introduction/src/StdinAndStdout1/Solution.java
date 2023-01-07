d0e02f658a508ff03e9e1969a8ebdda45542ffea
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-07 18:10:07 +0300

    Java Stdin - Stdout 1

---
+++ Introduction/src/StdinAndStdout1/Solution.java
@@ -0,0 +1,19 @@
+package StdinAndStdout1;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        int number3 = input.nextInt();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+    }
+}
