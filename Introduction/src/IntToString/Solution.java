5f590eda60c5da96ca8132fad7006002b1ad3273
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-07 23:31:45 +0300

    Int To String

---
+++ Introduction/src/IntToString/Solution.java
@@ -0,0 +1,16 @@
+package IntToString;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        String s = Integer.toString(x);
+        System.out.println("Good job");
+
+    }
+}
