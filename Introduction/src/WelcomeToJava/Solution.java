a0064bcf9af1f3360fc3b66ad6e054510e3bfbba
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-07 18:02:35 +0300

    Java If-Else

--- Introduction/src/WelcomeToJava/Solution.java
+++ Introduction/src/WelcomeToJava/Solution.java
@@ -1,7 +1,6 @@
-import java.io.*;
-import java.util.*;
+package WelcomeToJava;
 
-public class WelcomeToJava {
+public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
