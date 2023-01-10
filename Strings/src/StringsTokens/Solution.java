6d78fde14e6f4912142c49d3d1fbf511a695e8ae
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-11 00:00:54 +0300

    Strings Tokens

--- Strings/src/StringsTokens/Solution.java
+++ Strings/src/StringsTokens/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner input = new Scanner(System.in);
+        // Java'daki trim() metodu, bir dize içindeki baştaki ve sondaki boşlukları kaldırır
         String sentence = input.nextLine().trim();
 
         String[] tokens = sentence.split("[^\\p{Alpha}]+");
