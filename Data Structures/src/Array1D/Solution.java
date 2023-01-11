582ed58b82e3d3a206b85f99bf099da11b1306eb
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-11 22:45:53 +0300

    1D Array

---
+++ Data Structures/src/Array1D/Solution.java
@@ -0,0 +1,33 @@
+package Array1D;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] a = new int[n];
+        for(int i = 0; i<n; i++){
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
