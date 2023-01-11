81f2552497ae23c5afa464e5d058996b61ddf5ea
ibrahimbayburtlu <ibrahimbayburtlu5@gmail.com>
2023-01-11 22:33:46 +0300

    Big Integer

---
+++ bigNumber/src/BigInteger/Solution.java
@@ -0,0 +1,27 @@
+package BigInteger;
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        String number1 = input.next();
+        String number2 = input.next();
+
+        BigInteger multiply;
+        BigInteger sum;
+
+        BigInteger a = new BigInteger(number1);
+        BigInteger b = new BigInteger(number2);
+
+        multiply = a.multiply(b);
+        sum = a.add(b);
+
+        System.out.println(sum);
+        System.out.println(multiply);
+    }
+}
